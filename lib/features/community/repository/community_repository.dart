819f6584640b4b5dbc378c718d18119d5960b308
TheBoy-WhoCode <jiten.pateljp2001@gmail.com>
2022-11-16 22:00:12 +0530

    community created with bug

---
+++ lib/features/community/repository/community_repository.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:reddit_clone/core/constants/firebase_constants.dart';
+import 'package:reddit_clone/core/failure.dart';
+import 'package:reddit_clone/core/providers/firebase_provider.dart';
+import 'package:reddit_clone/core/type_defs.dart';
+import 'package:reddit_clone/models/community_model.dart';
+
+final communityRepositoryProvider = Provider<CommunityRepository>((ref) {
+  return CommunityRepository(firestore: ref.watch(firestoreProvider));
+});
+
+class CommunityRepository {
+  final FirebaseFirestore _firestore;
+  CommunityRepository({required FirebaseFirestore firestore})
+      : _firestore = firestore;
+
+  FutureVoid createCommunity(CommunityModel community) async {
+    try {
+      final communityDoc = await _communities.doc(community.name).get();
+      if (communityDoc.exists) {
+        throw 'Community with the same name already exists!';
+      }
+      return right(_communities.doc(community.name).set(community.toMap()));
+    } on FirebaseException catch (e) {
+      return left(Failure(e.message!));
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
+
+  CollectionReference get _communities =>
+      _firestore.collection(FirebaseConstants.communitiesCollection);
+}
--- lib/features/community/screens/create_community_screen.dart
+++ lib/features/community/screens/create_community_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:reddit_clone/features/community/controller/community_controller.dart';
+
+import '../../../core/common/loader.dart';
 
 class CreateCommunityScreen extends StatefulHookConsumerWidget {
   const CreateCommunityScreen({super.key});
@@ -11,55 +14,66 @@ class CreateCommunityScreen extends StatefulHookConsumerWidget {
 }
 
 class _CreateCommunityScreenState extends ConsumerState<CreateCommunityScreen> {
+  // late TextEditingController communityNameController;
+  void createCommunity(TextEditingController communityNameController) {
+    ref.read(communityControllerProvider.notifier).createCommunity(
+          communityNameController.text.trim(),
+          context,
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
     final communityNameController = useTextEditingController();
+    final isLoading = ref.watch(communityControllerProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Create a Community'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Text("Community name"),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextField(
-              controller: communityNameController,
-              decoration: const InputDecoration(
-                hintText: "r/Community_name",
-                filled: true,
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.all(18),
+      body: isLoading
+          ? const Loader()
+          : Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  const Align(
+                    alignment: Alignment.topLeft,
+                    child: Text("Community name"),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: communityNameController,
+                    decoration: const InputDecoration(
+                      hintText: "r/Community_name",
+                      filled: true,
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.all(18),
+                    ),
+                    maxLength: 21,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () => createCommunity(communityNameController),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: const Text(
+                      "Create Community",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  )
+                ],
               ),
-              maxLength: 21,
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              child: const Text(
-                "Create Community",
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
